c1e4a2cb2917e7c38b430685241b240b500dbe31
tex008 <texera009@gmail.com>
2023-09-14 16:12:54 -0300

    feat: create new route generate-ai-completion

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,5 +1,6 @@
 import { fastify } from 'fastify';
 import { createTranscriptionRoute } from './routes/create-transcription';
+import { generateAiCompletionRoute } from './routes/generate-ai-completion';
 import { getAllPromptsRoute } from './routes/get-all-prompts';
 import { uploadVideoRoute } from './routes/upload-video';
 
@@ -9,6 +10,7 @@ const app = fastify()
 app.register(getAllPromptsRoute)
 app.register(uploadVideoRoute)
 app.register(createTranscriptionRoute)
+app.register(generateAiCompletionRoute)
 
 app.listen({
   port: 3333,
